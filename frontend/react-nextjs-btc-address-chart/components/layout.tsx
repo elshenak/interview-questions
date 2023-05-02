c998e56bea37ff79f5edf4e94c78079b41033acf
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-05-02 10:16:15 -0500

    fix name

--- frontend/react-nextjs-btc-address-chart/components/layout.tsx
+++ frontend/react-nextjs-btc-address-chart/components/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({
         <div className="mx-auto max-w-7xl px-6 lg:px-8 py-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
-              Blockworks Interview Challenge
+              NibiruChain Interview Challenge
             </h2>
             <p className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               BTC Address Balances over Time
